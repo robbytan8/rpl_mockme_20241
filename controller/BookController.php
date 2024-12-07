a9db9db2de03023151a745ad76c276bba5cdc4dd
Robby Tan <robby-tan@outlook.com>
2024-12-08 01:00:10 +0700

    refrac: cover validation and new file name

--- controller/BookController.php
+++ controller/BookController.php
@@ -32,33 +32,7 @@ class BookController
       $errMessage = 'Please fill all the required fields';
       header('location: index.php?menu=adm-book-create&message=' . $errMessage);
     } else {
-      if (isset($_FILES['cover']['name']) && $_FILES['cover']['name'] != '') {
-        $targetDir = 'uploads/cover/';
-        $fileValid = 1;
-        $imageType = strtolower(pathinfo($_FILES['cover']['name'], PATHINFO_EXTENSION));
-
-        //  1st Validation (Check is image)
-        $isImage = getimagesize($_FILES['cover']['tmp_name']);
-        if ($isImage === false) {
-          $fileValid = 0;
-        }
-
-        //  2nd Validation (Check extension)
-        if ($fileValid && ($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg")) {
-          $fileValid = 0;
-        }
-
-        //  3rd Validation (Check file size)
-        if ($fileValid && $_FILES['cover']['size'] > 2 * 1024 * 1024) {
-          $fileValid = 0;
-        }
-
-        //  Upload process
-        if ($fileValid) {
-          $newFileName = $isbn13 . '.' . $imageType;
-          move_uploaded_file($_FILES['cover']['tmp_name'], $targetDir . $newFileName);
-        }
-      }
+      $newFileName = $this->validateAndGetCoverName($isbn13);
       $book = new Book();
       $book->setIsbn13($isbn13);
       $book->setTitle($title);
@@ -109,33 +83,7 @@ class BookController
           unlink('uploads/cover/' . $book->getCover());
         }
 
-        if (isset($_FILES['cover']['name']) && $_FILES['cover']['name'] != '') {
-          $targetDir = 'uploads/cover/';
-          $fileValid = 1;
-          $imageType = strtolower(pathinfo($_FILES['cover']['name'], PATHINFO_EXTENSION));
-
-          //  1st Validation (Check is image)
-          $isImage = getimagesize($_FILES['cover']['tmp_name']);
-          if ($isImage === false) {
-            $fileValid = 0;
-          }
-
-          //  2nd Validation (Check extension)
-          if ($fileValid && ($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg")) {
-            $fileValid = 0;
-          }
-
-          //  3rd Validation (Check file size)
-          if ($fileValid && $_FILES['cover']['size'] > 2 * 1024 * 1024) {
-            $fileValid = 0;
-          }
-
-          //  Upload process
-          if ($fileValid) {
-            $newFileName = $isbn13 . '.' . $imageType;
-            move_uploaded_file($_FILES['cover']['tmp_name'], $targetDir . $newFileName);
-          }
-        }
+        $newFileName = $this->validateAndGetCoverName($isbn13);
         $book->setTitle($title);
         $book->setAuthor($author);
         $book->setPublisher($publisher);
@@ -156,5 +104,42 @@ class BookController
       $errMessage = 'Book not found';
       header('location: index.php?menu=adm-book&err_message=' . $errMessage);
     }
+  }
+
+  /**
+   * @param $isbn13
+   * @return string
+   */
+  private function validateAndGetCoverName($isbn13): ?string
+  {
+    $newFileName = null;
+    if (isset($_FILES['cover']['name']) && $_FILES['cover']['name'] != '') {
+      $targetDir = 'uploads/cover/';
+      $fileValid = 1;
+      $imageType = strtolower(pathinfo($_FILES['cover']['name'], PATHINFO_EXTENSION));
+
+      //  1st Validation (Check is image)
+      $isImage = getimagesize($_FILES['cover']['tmp_name']);
+      if ($isImage === false) {
+        $fileValid = 0;
+      }
+
+      //  2nd Validation (Check extension)
+      if ($fileValid && ($imageType != "jpg" && $imageType != "png" && $imageType != "jpeg")) {
+        $fileValid = 0;
+      }
+
+      //  3rd Validation (Check file size)
+      if ($fileValid && $_FILES['cover']['size'] > 2 * 1024 * 1024) {
+        $fileValid = 0;
+      }
+
+      //  Upload process
+      if ($fileValid) {
+        $newFileName = $isbn13 . '.' . $imageType;
+        move_uploaded_file($_FILES['cover']['tmp_name'], $targetDir . $newFileName);
+      }
+    }
+    return $newFileName;
   }
 }
